cd55dc99e87f32ca17838be650c2ab462107e720
Neshab Ansari <neshabalam8008@gmail.com>
2024-11-01 21:21:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from 'react';
 
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); // whether dark mode is enabled or not
@@ -41,17 +41,17 @@ function App() {
 
   return (
     <>
-    {/* <Router> */}
+    <Router> 
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      {/* <Alert alert={alert} /> */}
+      <Alert alert={alert} />
       <div className="container my-3">
-        {/* <Routes>  */}
-          {/* <Route exact path="/about" element={<About />} /> */}
-          {/* <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} /> */}
-          <TextForm showAlert={showAlert} heading="Enter text which you want to modified" mode={mode} />
-         {/* </Routes> */}
+        <Routes> 
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />} />
+          {/* <TextForm showAlert={showAlert} heading="Enter text which you want to modified" mode={mode} /> */}
+         </Routes>
       </div>
-    {/* </Router>  */}
+    </Router> 
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Navbar(props) {
   return (
     
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" to="#">
             {props.title}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -24,16 +24,11 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                {/* <Link className="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link> */}
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
               </li>
-              {/* <li className="nav-item">
-                <Link className="nav-link" to="/about">
-                  {props.aboutText}
-                </Link>
-              </li> */}
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">{props.aboutText}</Link>
+              </li>
             </ul>
             <form className="d-flex">
               <input
